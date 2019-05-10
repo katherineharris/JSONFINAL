04e8e9ee52c2b011fd80fb1fd2da00eab2e02741
Katie Harris <katherineharris@ou.edu>
2019-05-10 00:11:04 -0500

    KATIE V HARRIS

---
+++ JSON/MVC/Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Schedule
+        public ActionResult Index(int year = 2019)
+        {
+            var date = db.FootballSchedules.Where(x => x.Season == year);
+            return View(date);
+        }
+    }
+}
